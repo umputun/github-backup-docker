f8462fa894dfba8197d1242c420588a4d5d30c38
Umputun <umputun@gmail.com>
2019-04-08 17:57:44 -0500

    missing TZ

--- exec.sh
+++ exec.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+TIME_ZONE=${TIME_ZONE:=UTC}
+echo "timezone=${TIME_ZONE}"
+cp /usr/share/zoneinfo/${TIME_ZONE} /etc/localtime
+echo "${TIME_ZONE}" >/etc/timezone
+date
+
 echo "$(date) - start backup scheduler"
 while :; do
     DATE=$(date +%Y%m%d-%H%M%S)

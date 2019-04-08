2d4765ffc013e2935f2b3e79f7cb4b4b679687fd
Umputun <umputun@gmail.com>
2019-04-08 17:43:14 -0500

    hide stderr on cleanups

--- exec.sh
+++ exec.sh
@@ -7,8 +7,8 @@ while :; do
     github-backup ${GITHUB_USER} --token=$TOKEN --all --output-directory=/srv/var/${DATE} --private
 
     echo "$(date) - cleanup"
-    find /srv/var/ -mtime +${MAX_BACKUPS} -exec rm -fv {} \;
-    find /srv/var/ -type d -exec rmdir {} \;
+    find /srv/var/ -mtime +${MAX_BACKUPS} -exec rm -fv {} \; 2>/dev/null
+    find /srv/var/ -type d -exec rmdir {} \; 2>/dev/null
 
     echo "$(date) - sleep for 1 day"
     sleep 1d

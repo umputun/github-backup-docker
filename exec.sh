22a1d093a05dfed57560770f8d388539c209dc9e
Umputun <umputun@gmail.com>
2019-04-08 20:40:30 -0500

    allow multiple users with ,

--- exec.sh
+++ exec.sh
@@ -4,13 +4,15 @@ TIME_ZONE=${TIME_ZONE:=UTC}
 echo "timezone=${TIME_ZONE}"
 cp /usr/share/zoneinfo/${TIME_ZONE} /etc/localtime
 echo "${TIME_ZONE}" >/etc/timezone
-date
 
 echo "$(date) - start backup scheduler"
 while :; do
     DATE=$(date +%Y%m%d-%H%M%S)
-    echo "$(date) - execute backup for ${DATE}"
-    github-backup ${GITHUB_USER} --token=$TOKEN --all --output-directory=/srv/var/${DATE} --private --gists
+
+    for u in $(echo $GITHUB_USER | tr "," "\n"); do
+        echo "$(date) - execute backup for ${u}, ${DATE}"
+        github-backup ${u} --token=$TOKEN --all --output-directory=/srv/var/${DATE}/${u} --private --gists
+    done
 
     echo "$(date) - cleanup"
     find /srv/var/ -mtime +${MAX_BACKUPS} -exec rm -fv {} \; 2>/dev/null

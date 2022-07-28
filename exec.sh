e5aafb8adde097ddeea525ffd793fc3942f6c01f
Christian Foellmann <christian@foellmann.de>
2022-07-28 09:49:28 +0200

    add backups for ORG

--- exec.sh
+++ exec.sh
@@ -9,11 +9,33 @@ echo "$(date) - start backup scheduler"
 while :; do
     DATE=$(date +%Y%m%d-%H%M%S)
 
-    for u in $(echo $GITHUB_USER | tr "," "\n"); do
-        echo "$(date) - execute backup for ${u}, ${DATE}"
+	if [ -z "$GITHUB_USER" ]
+	  then
+      echo "No Github users defined."
+    else
+      for u in $(echo $GITHUB_USER | tr "," "\n"); do
+        echo "$(date) - execute backup for User ${u}, ${DATE}"
         github-backup ${u} --token=$TOKEN --all --output-directory=/srv/var/${DATE}/${u} --private --gists
-    done
+      done
+  fi
 
+	if [ -z "$GITHUB_ORG" ]
+	  then
+          echo "No Github organization defined."
+    else
+      for o in $(echo $GITHUB_ORG | tr "," "\n"); do
+        echo "$(date) - execute backup for Organization ${u}, ${DATE}"
+        github-backup ${o} --organization --token=$TOKEN --all --output-directory=/srv/var/${DATE}/${o} --private --gists
+      done
+	fi
+
+	if [ -z "$GITHUB_USER" ] && [ -z "$GITHUB_ORG" ]
+    then
+      echo "No entities (user, organization) defined. No backup performed."
+    else
+      echo "Backup performed."
+  fi
+    
     echo "$(date) - cleanup"
 
     ls -d1 /srv/var/* | head -n -${MAX_BACKUPS} | xargs rm -rf
